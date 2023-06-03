84637bb0bf4363b5a9ab990d578b32a7643ba80e
Raaed Serag <raaedserag@gmail.com>
2023-06-03 18:20:18 +0300

    add support to unit testing

--- ping-pong-service-repo/app/src/routes/landing-routes.js
+++ ping-pong-service-repo/app/src/routes/landing-routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import * as healthCheckControllers from "../controllers/health-checks-controller.js"
+import {getCurrentTime} from "../helpers/time-helper.js"
 const router = Router();
+
+router.get("/", (req, res) => res.send(`Welcome to ping pong HTTP service, current server time is ${getCurrentTime()}`));
 router.get("/ping", healthCheckControllers.checkLiveStatus);
 export default router;
