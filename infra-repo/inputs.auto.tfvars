1cb1283fbaf63f2faffb82480c0eb9ea4d67327f
Raaed Serag <raaedserag@gmail.com>
2023-06-03 19:52:13 +0300

    Add vpc module

---
+++ infra-repo/inputs.auto.tfvars
@@ -0,0 +1,5 @@
+vpc_cidr_block              = "10.0.0.0/16"
+subnet_public_a_cidr_block  = "10.0.1.0/24"
+subnet_public_b_cidr_block  = "10.0.2.0/24"
+subnet_private_a_cidr_block = "10.0.11.0/24"
+subnet_private_b_cidr_block = "10.0.21.0/24"
--- infra-repo/main.tf
+++ infra-repo/main.tf
@@ -10,3 +10,14 @@ terraform {
 }
 
 provider "aws" {}
+
+# VPC Module
+# As an alternative to this module we can use the AWS VPC module(https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest)
+module "eks_vpc" {
+  source                      = "./modules/00_AWS-EKS-Vpc-2Azs-4Subnets"
+  vpc_cidr_block              = var.vpc_cidr_block
+  subnet_public_a_cidr_block  = var.subnet_public_a_cidr_block
+  subnet_private_a_cidr_block = var.subnet_private_a_cidr_block
+  subnet_public_b_cidr_block  = var.subnet_public_b_cidr_block
+  subnet_private_b_cidr_block = var.subnet_private_b_cidr_block
+}
