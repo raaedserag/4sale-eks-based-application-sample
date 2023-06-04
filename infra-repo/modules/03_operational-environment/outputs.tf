57e1e833262f7ef218991775fe9c2cf94d10bf7f
Raaed Serag <raaedserag@gmail.com>
2023-06-04 23:42:27 +0300

    add environment config to secrets manager

--- infra-repo/modules/03_operational-environment/outputs.tf
+++ infra-repo/modules/03_operational-environment/outputs.tf
@@ -8,3 +8,11 @@ resource "aws_secretsmanager_secret_version" "mysql_config" {
 }
 
 
+# Export Environment Config to AWS Secrets Manager
+resource "aws_secretsmanager_secret" "environment_config" {
+  name = "/${var.namespace}/${var.environment_name}/environment-config"
+}
+resource "aws_secretsmanager_secret_version" "environment_config" {
+  secret_id     = aws_secretsmanager_secret.environment_config.id
+  secret_string = jsonencode(local.environment_config)
+}
