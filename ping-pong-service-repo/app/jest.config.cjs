1a20b9cba88da66e550d6d07e7160050d0cd412d
Raaed Serag <raaedserag@gmail.com>
2023-06-08 20:27:14 +0300

    fix unit testing output file

--- ping-pong-service-repo/app/jest.config.cjs
+++ ping-pong-service-repo/app/jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
         'default',
         ['jest-junit', {
             outputDirectory: "./test-results/jest",
-            outputName: "ping-pong-service-results.xml"
+            outputName: "unit-testing-results.xml"
         }]
     ]
 }
