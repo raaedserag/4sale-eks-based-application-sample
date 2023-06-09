67c80cb570bd48c300ec631f6d30e0c3eb265c92
Raaed Serag <raaedserag@gmail.com>
2023-06-09 17:26:13 +0300

    fix sql statement error

--- ping-pong-service-repo/app/src/db/sequelize-client.js
+++ ping-pong-service-repo/app/src/db/sequelize-client.js
@@ -28,7 +28,8 @@ class MysqlClientConnection {
                 user: mysqlConfiguration.user,
                 password: mysqlConfiguration.password,
             });
-            await initialConnection.query(`CREATE DATABASE IF NOT EXISTS ${mysqlConfiguration.database};`);
+            await initialConnection.query(`CREATE DATABASE IF NOT EXISTS "${mysqlConfiguration.database}";`);
+            await initialConnection.end();
             await this.ping();
 
         } catch (error) {
